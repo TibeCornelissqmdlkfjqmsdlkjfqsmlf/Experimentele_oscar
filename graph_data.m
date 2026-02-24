de613344c429eb6eb307ed380b17e1c4df07346a
Tibe_Cornelis <tibe.cornelis@student.uhasselt.be>
2026-02-24 14:20:50 +0100

    mod number of sweeps

---
+++ graph_data.m
@@ -0,0 +1,5 @@
+odmr_v = odmr(:);
+sweep_v = sweep(:);
+
+plot(odmr_v,sweep_v);
+    
--- python/main.py
+++ python/main.py
@@ -22,9 +22,11 @@ def load_decimal_comma_stream(path: str | Path) -> np.ndarray:
     return vals
 
 fold_front = r"D:\Jelle_Tibe_Experimentele\ruwe_data\mod-mr\cf_2870-md_65-dbm_16-N_1-ds_600-mr_"
-freq = ["1","1,1","1,2","1,3","1,4","1,5","1,6","1,7","1,8","1,9","2","2,1","2,2","2,3","2,4","2,5","2,6","2,7","2,8","2,9","3"]
+freq = ["1","3","4","5"]
 
-folders = [fold_front + f for f in freq ]
+fold_back = ""
+
+folders = [fold_front + f + fold_back for f in freq ]
 plt.figure()
 
 for folder, f_label in zip(folders, freq):
